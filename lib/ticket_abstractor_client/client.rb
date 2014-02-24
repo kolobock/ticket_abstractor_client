8a74b011526547b869adfd2957eb91621f02417b
Andrii Rudenko <arudenko@productengine.com>
2014-02-24 15:04:27 +0200

    add support for security token on client initialization

--- lib/ticket_abstractor_client/client.rb
+++ lib/ticket_abstractor_client/client.rb
@@ -1,10 +1,12 @@
 class TicketAbstractorClient::Client
-  def initialize(base_url)
+  def initialize(base_url, security_token=nil)
     @base_url = base_url
+    @security_token = security_token
   end
 
   def get(url, args = {}, params = {})
     params.merge! args: args.to_json
+    params.merge! security_token: @security_token
     response = RestClient.get("#{@base_url}/#{url}", params: params)
 
     JSON.parse response
@@ -12,6 +14,7 @@ class TicketAbstractorClient::Client
 
   def post(url, args = {}, params = {})
     params.merge! args: args.to_json
+    params.merge! security_token: @security_token
     response = RestClient.post("#{@base_url}/#{url}", params)
 
     JSON.parse response
